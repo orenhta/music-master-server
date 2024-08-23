d0f7d562bd5348a10f2956388096c747dac9f007
Omri Alster <omrialster@gmail.com>
2024-08-23 18:38:33 +0300

    fix bug - when score object is empty

--- src/functions/game-state-to-end-round-score.ts
+++ src/functions/game-state-to-end-round-score.ts
@@ -1,12 +1,15 @@
-import { EndRoundPlayerRespnse } from "src/types/end-round-response.type";
-import { GameState, RoundData } from "src/types/game-state.type";
+import { EndRoundPlayerRespnse } from 'src/types/end-round-response.type';
+import { GameState, RoundData } from 'src/types/game-state.type';
 
-export const gameStateToEndRoundScore =(
-    gameState: GameState
-  ): EndRoundPlayerRespnse[] => {
-    return Object.values(gameState.gamePlayers).map((player) => ({
-        ...player,
-        gainedScore: (gameState.roundData as RoundData)
-        .scores[player.userName] ?? 0,
-      }))
-  };
+export const gameStateToEndRoundScore = (
+  gameState: GameState,
+): EndRoundPlayerRespnse[] => {
+  const roundScores = (gameState.roundData as RoundData).scores;
+  return Object.values(gameState.gamePlayers).map((player) => ({
+    ...player,
+    gainedScore:
+      roundScores && roundScores[player.userName]
+        ? roundScores[player.userName]
+        : 0,
+  }));
+};

8fc0717fd980965875bdcfb7d5cd86a4b24b3239
Omri Alster <omrialster@gmail.com>
2024-08-23 12:55:24 +0300

    remove redirect uri

--- src/config/spotify.config.ts
+++ src/config/spotify.config.ts
@@ -13,7 +13,6 @@ export const spotifyConfig = registerAs('spotifyConfig', () => {
   return {
     clientId: process.env.SPOTIFY_CLIENT_ID!,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
     playlistIdByGenre,
   };
 });
